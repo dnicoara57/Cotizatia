29645ee709feee4f1b6f85747eac48c5cff20606
dnicoara57 <nicadialex@yahoo.com>
2025-09-11 17:10:43 +0300

    Am revenit la androidx.activity:activity:1.10.1  si am mai modificat codul ca butoanele din partea de jos a ecranului sa fie putin mai sus pentru a nu fi acoperite de bara de stare daca aceasta exista

--- app/src/main/java/com/danielapps/cotizatia/MainActivity.java
+++ app/src/main/java/com/danielapps/cotizatia/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.AbsoluteSizeSpan;
-import android.text.style.RelativeSizeSpan;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -94,9 +93,6 @@ public class MainActivity extends AppCompatActivity {
         btnPlataOnline = findViewById(R.id.btnPlataOnline);
         btnBalantaDetalii = findViewById(R.id.btnBalantaDetalii);
 
-        //setare marime text in hint anul
-        //SpannableString hint = new SpannableString(getString(R.string.anul));
-        //hint.setSpan(new RelativeSizeSpan(1.2f), 0, hint.length(), 0); // mărește hint-ul cu 20%
 
         float hintSizePx = getResources().getDimension(R.dimen.text_body_size); // dimensiunea în px
         SpannableString hint = new SpannableString(getString(R.string.anul));
